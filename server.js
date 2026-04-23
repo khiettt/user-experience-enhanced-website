d7005584ac454303187c38ccf2fe11738bd1ff98
khiettt <khitam.bakkali.nebkhout@hva.nl>
2026-04-23 20:02:42 +0200

    #6 Url gefetched en de parameters toegevoegd

--- server.js
+++ server.js
@@ -2,4 +2,9 @@ console.log('Hier komt je server voor Sprint 10.')
 
 console.log('Gebruik uit Sprint 9 alleen de code die je mee wilt nemen.')
 
-console.log('Zet \'m op!')
+console.log('Zet \'m op!')app.get('/', async function (request, response) {
+  const parms = {
+    'fields': 'name,image,shop_url,description,shop_name,slug,url,amount, id',
+    'filter[tags][_icontains]': 'moederdag',
+    'fields': '*,img.width,img.height'
+  }
